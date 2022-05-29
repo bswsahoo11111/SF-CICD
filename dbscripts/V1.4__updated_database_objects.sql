6d89c2b7ae690dbbdc6025d90262d650cb048980
unknown <bswsahoo11111@gmail.com>
2022-05-29 11:17:05 +0530

    'new25'

--- dbscripts/V1.4__updated_database_objects.sql
+++
@@ -1,3 +0,0 @@
-use database DEMO3;
-use schema DEMO3;
-insert into FIRST_TABLE_CICD values('ravi','arora');
---
+++ dbscripts/V1.5__updated_database_objects.sql
@@ -0,0 +1,3 @@
+use database DEMO3;
+use schema DEMO3;
+delete from FIRST_TABLE_CICD where FIRST_NAME='ravi';
